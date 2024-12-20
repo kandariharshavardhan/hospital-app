a3610d8f262a64b97530c989be125f59f009f935
kandariharshavardhan@gmail.com <kandariharshavardhan@gmail.com>
2024-12-20 10:50:23 +0530

    Initial commit

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,7 +2,7 @@ import { Button, Box, Heading } from '@chakra-ui/react';
 function About() {
     return (
       <Box>
-        <h1>Contact Us</h1>
+        <h1>Contactss Us</h1>
         <p>Reach out to us via email or phone.</p>
       </Box>
     );
